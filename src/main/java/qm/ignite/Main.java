63a3115557f16ec652fca662ccd019c4c3a2909b
Michael Qvortrup <qm@zuehlke.com>
2018-02-14 14:11:47 +0100

    Initial commit

---
+++ src/main/java/qm/ignite/Main.java
@@ -0,0 +1,11 @@
+package qm.ignite;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ignite ignite = Ignition.start();
+    }
+}
